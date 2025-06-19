76b12d7a279b3d022beb8c9c197820077e3e3700
Fernando <silentguyband@gmail.com>
2025-06-19 14:46:36 -0300

    Criando a camada de infraestrutura

--- src/main/java/com/fernandes/email_service/application/EmailSenderService.java
+++ src/main/java/com/fernandes/email_service/application/EmailSenderService.java
@@ -1,10 +1,12 @@
 package com.fernandes.email_service.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.fernandes.email_service.adapters.EmailSenderGateway;
 import com.fernandes.email_service.core.EmailSanderUseCase;
 
+@Service
 public class EmailSenderService implements EmailSanderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
