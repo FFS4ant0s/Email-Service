0544c8f9b6d7713151249b17b7ea35c3a446e5aa
Fernando <silentguyband@gmail.com>
2025-06-19 14:30:18 -0300

    Criando adapters

---
+++ src/main/java/com/fernandes/email_service/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package com.fernandes.email_service.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fernandes.email_service.adapters.EmailSenderGateway;
+import com.fernandes.email_service.core.EmailSanderUseCase;
+
+public class EmailSenderService implements EmailSanderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+
+}
